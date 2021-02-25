435e90cfc95f7b3f0c057b322db765f0f830e183
zvanderslice <62152014+zvanderslice@users.noreply.github.com>
2021-02-25 08:57:53 -0600

    Add files via upload

---
+++ Loan.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of class Loan here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Loan
+{
+    // instance variables - replace the example below with your own
+    private int ID;
+    private double loanAmount;
+    private String country;
+    private int daysToFund;
+    private int numLenders;
+
+        public Loan(int ID, double loanAmount, String country, int daysToFund, 
+                int numLenders) {
+       this.ID = ID;
+       this.loanAmount = loanAmount;
+       this.country = country;
+       this.daysToFund = daysToFund;
+       this.numLenders = numLenders;
+    }
+
+}
